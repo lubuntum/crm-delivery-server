4b6957d5c90201b661b2108e83786ac2fdffad08
lubuntum <lubuntum@gmail.com>
2025-04-06 01:01:32 +0700

    Поиск организации по id

--- src/main/java/com/delivery/mydelivery/database/services/organization/OrganizationService.java
+++ src/main/java/com/delivery/mydelivery/database/services/organization/OrganizationService.java
@@ -1,5 +1,6 @@
 package com.delivery.mydelivery.database.services.organization;
 
+import com.delivery.mydelivery.database.entities.organization.Organization;
 import com.delivery.mydelivery.database.repositories.ordanization.OrganizationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,8 @@ import org.springframework.stereotype.Service;
 public class OrganizationService {
     @Autowired
     OrganizationRepository organizationRepository;
+
+    public Organization getOrganizationById(Long id) {
+        return organizationRepository.findById(id).orElseThrow(NullPointerException::new);
+    }
 }

063f18659d47a245568604d372063c63d12fa0c6
lubuntum <lubuntum@gmail.com>
2025-04-06 01:03:59 +0700

    Добавлен метод получения данных аккаунта, обновления пароля

--- src/main/java/com/delivery/mydelivery/database/repositories/accountmanage/AccountRepository.java
+++ src/main/java/com/delivery/mydelivery/database/repositories/accountmanage/AccountRepository.java
@@ -1,10 +1,23 @@
 package com.delivery.mydelivery.database.repositories.accountmanage;
 
 import com.delivery.mydelivery.database.entities.accountmanage.Account;
+import com.delivery.mydelivery.dto.auth.AccountData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findByEmail(String email);
+
+    @Query("SELECT new com.delivery.mydelivery.dto.auth.AccountData(" +
+            "a.id, a.email, r.name, " +
+            "e.name, e.secondName, e.patronymic, " +
+            "e.phone, o.name) " +
+            "FROM Account a " +
+            "INNER JOIN role r " +
+            "INNER JOIN employee e " +
+            "INNER JOIN organization o " +
+            "WHERE a.id = :id")
+    AccountData findAccountDataById(Long id);
 }
--- src/main/java/com/delivery/mydelivery/database/services/accountmanage/AccountService.java
+++ src/main/java/com/delivery/mydelivery/database/services/accountmanage/AccountService.java
@@ -2,6 +2,7 @@ package com.delivery.mydelivery.database.services.accountmanage;
 
 import com.delivery.mydelivery.database.entities.accountmanage.Account;
 import com.delivery.mydelivery.database.repositories.accountmanage.AccountRepository;
+import com.delivery.mydelivery.dto.auth.AccountData;
 import com.delivery.mydelivery.dto.auth.AuthCredential;
 import com.delivery.mydelivery.services.jwt.JwtService;
 import com.delivery.mydelivery.utility.password.PasswordValidationUtil;
@@ -15,12 +16,22 @@ public class AccountService {
     @Autowired
     private JwtService jwtService;
 
+    //TODO make custom exception, now returned 200 with empty request if some error occurred
     public String validateAuthCredential(AuthCredential authCredential) {
         Account account = accountRepository.findByEmail(authCredential.getEmail());
         if (account == null) return null;
         if(!PasswordValidationUtil.validatePassword(authCredential.getPassword(), account.getPassword()))
             return null;
         return jwtService.generateToken(String.valueOf(account.getId()));
+    }
+    public AccountData getAccountData(Long id) {
+        return accountRepository.findAccountDataById(id);
+    }
+    public Boolean updatePassword(Long id, String updatedPass) {
+        Account account = accountRepository.findById(id).orElseThrow(NullPointerException::new);
+        account.setPassword(PasswordValidationUtil.hashPassword(updatedPass));
+        accountRepository.save(account);
+        return true;
     }
     //TODO Unstable test
     public Account createAccount(Account account) {

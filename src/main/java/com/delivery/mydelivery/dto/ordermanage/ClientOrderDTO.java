371ee6868e98641a69071d9db89bfdeb9c29c9e0
lubuntum <lubuntum@gmail.com>
2025-04-06 01:00:03 +0700

    Обновлен DTO класс для создания заказов

--- src/main/java/com/delivery/mydelivery/dto/ordermanage/ClientOrderDTO.java
+++ src/main/java/com/delivery/mydelivery/dto/ordermanage/ClientOrderDTO.java
@@ -1,11 +1,12 @@
 package com.delivery.mydelivery.dto.ordermanage;
 
+import com.delivery.mydelivery.database.entities.accountmanage.Client;
 import com.delivery.mydelivery.database.entities.ordermanage.status.StatusEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -14,11 +15,22 @@ public class ClientOrderDTO {
     private Long id;
     private String address;
     private String comment;
-    private LocalDate createdAt;
+    private LocalDateTime createdAt;
     private StatusEnum status;
+    private Long clientId;
     private String clientName;
     private String clientSecondName;
     private String clientPatronymic;
     private String clientEmail;
     private String clientPhone;
+
+    public Client extractClientData(){
+        Client client = new Client();
+        client.setName(clientName);
+        client.setSecondName(clientSecondName);
+        client.setPatronymic(clientPatronymic);
+        client.setEmail(clientEmail);
+        client.setPhone(clientPhone);
+        return client;
+    }
 }

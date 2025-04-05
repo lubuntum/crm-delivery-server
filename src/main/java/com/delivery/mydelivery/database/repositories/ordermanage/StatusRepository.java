0a8ed54cb6cf5a90809b14994802242aa06e50d4
lubuntum <lubuntum@gmail.com>
2025-04-06 01:00:57 +0700

    Добавлен поиск статуса по названию

--- src/main/java/com/delivery/mydelivery/database/repositories/ordermanage/StatusRepository.java
+++ src/main/java/com/delivery/mydelivery/database/repositories/ordermanage/StatusRepository.java
@@ -1,7 +1,9 @@
 package com.delivery.mydelivery.database.repositories.ordermanage;
 
 import com.delivery.mydelivery.database.entities.ordermanage.status.Status;
+import com.delivery.mydelivery.database.entities.ordermanage.status.StatusEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StatusRepository extends JpaRepository<Status, Long> {
+    Status findByName(StatusEnum statusName);
 }
--- src/main/java/com/delivery/mydelivery/database/services/ordermanage/StatusService.java
+++ src/main/java/com/delivery/mydelivery/database/services/ordermanage/StatusService.java
@@ -1,5 +1,7 @@
 package com.delivery.mydelivery.database.services.ordermanage;
 
+import com.delivery.mydelivery.database.entities.ordermanage.status.Status;
+import com.delivery.mydelivery.database.entities.ordermanage.status.StatusEnum;
 import com.delivery.mydelivery.database.repositories.ordermanage.StatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,8 @@ import org.springframework.stereotype.Service;
 public class StatusService {
     @Autowired
     StatusRepository statusRepository;
+
+    public Status getStatusByName(StatusEnum statusName) {
+        return statusRepository.findByName(statusName);
+    }
 }

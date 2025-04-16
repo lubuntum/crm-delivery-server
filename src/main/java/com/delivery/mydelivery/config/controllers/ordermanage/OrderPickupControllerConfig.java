efa9edc652a2caec7c01f643b75fec60dbbe4458
lubuntum <lubuntum@gmail.com>
2025-04-16 16:38:03 +0700

    Начальная конфигурация и сам контроллер для работы с вывозом заказов от киента

---
+++ src/main/java/com/delivery/mydelivery/config/controllers/ordermanage/OrderPickupControllerConfig.java
@@ -0,0 +1,18 @@
+package com.delivery.mydelivery.config.controllers.ordermanage;
+
+import com.delivery.mydelivery.config.interceptors.JwtValidationInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class OrderPickupControllerConfig implements WebMvcConfigurer {
+    @Autowired
+    private JwtValidationInterceptor jwtValidationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtValidationInterceptor).addPathPatterns("/api/orders-pickup/**");
+    }
+}
---
+++ src/main/java/com/delivery/mydelivery/controllers/ordermanage/PickupOrderController.java
@@ -0,0 +1,31 @@
+package com.delivery.mydelivery.controllers.ordermanage;
+
+import com.delivery.mydelivery.database.projections.OrderPickupProjection;
+import com.delivery.mydelivery.database.services.ordermanage.OrderPickupService;
+import com.delivery.mydelivery.dto.ordermanage.OrderPickupDTO;
+import com.delivery.mydelivery.services.jwt.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/orders-pickup")
+public class PickupOrderController {
+    @Autowired
+    private OrderPickupService orderPickupService;
+    @Autowired
+    private JwtService jwtService;
+    @PostMapping("/create")
+    public ResponseEntity<Long> createOrderPickup(
+            @RequestHeader("Authorization") String token,
+            @RequestBody OrderPickupDTO orderPickupDTO){
+        //TODO think, may be logic of extraction token data transfer to
+        // interceptor which append request with extracted data
+        orderPickupDTO.setCourierId(Long.valueOf(jwtService.extractSubject(token)));
+        return ResponseEntity.ok(orderPickupService.createOrderPickup(orderPickupDTO));
+    }
+    @GetMapping("/order-pickup/{id}")
+    public ResponseEntity<OrderPickupProjection> getPickupOrderById(@PathVariable Long id){
+        return ResponseEntity.ok(orderPickupService.getOrderPickupById(id));
+    }
+}

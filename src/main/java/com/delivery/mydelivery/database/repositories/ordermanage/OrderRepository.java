41f23c86e1bacf719c467538743a710ac9aa2dcd
lubuntum <lubuntum@gmail.com>
2025-04-16 16:39:23 +0700

    Запрос для DTO в заказах

--- src/main/java/com/delivery/mydelivery/database/repositories/ordermanage/OrderRepository.java
+++ src/main/java/com/delivery/mydelivery/database/repositories/ordermanage/OrderRepository.java
@@ -16,4 +16,11 @@ public interface OrderRepository extends JpaRepository<ClientOrder, Long> {
             "JOIN co.client c " +
             "WHERE co.organization.id=:organizationId")
     List<ClientOrderDTO> findByOrganizationId(@Param("organizationId") Long organizationId);
+    @Query("SELECT new com.delivery.mydelivery.dto.ordermanage.ClientOrderDTO( " +
+            "co.id, co.address, co.comment, co.createdAt, co.status.name, " +
+            "c.id ,c.name, c.secondName, c.patronymic, c.email, c.phone, co.organization.id ) " +
+            "FROM ClientOrder co " +
+            "JOIN co.client c " +
+            "WHERE co.id=:id")
+    ClientOrderDTO findDTOById(Long id);
 }

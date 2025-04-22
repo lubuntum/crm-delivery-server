99fe04a29d1f4bc7b2111e08b2d5cf4ea51a3ee2
lubuntum <lubuntum@gmail.com>
2025-04-22 19:37:12 +0700

    Entity для материалов

--- src/main/java/com/delivery/mydelivery/database/entities/productmanage/Material.java
+++ src/main/java/com/delivery/mydelivery/database/entities/productmanage/Material.java
@@ -1,4 +1,27 @@
 package com.delivery.mydelivery.database.entities.productmanage;
 
+import com.delivery.mydelivery.database.entities.organization.Organization;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "material")
 public class Material {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "organization_id", updatable = false, insertable = false)
+    private Long organizationId;
+    @ManyToOne
+    @JoinColumn(name = "organization_id", nullable = false)
+    private Organization organization;
+
 }

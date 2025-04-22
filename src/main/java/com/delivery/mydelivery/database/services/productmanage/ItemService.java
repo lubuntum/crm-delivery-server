499dbdeb63acd0436d8693f90a52d3f430ffacb1
lubuntum <lubuntum@gmail.com>
2025-04-22 19:37:03 +0700

    Сервис и маппер для позиций

---
+++ src/main/java/com/delivery/mydelivery/database/services/productmanage/ItemService.java
@@ -0,0 +1,36 @@
+package com.delivery.mydelivery.database.services.productmanage;
+
+import com.delivery.mydelivery.database.entities.ordermanage.ClientOrder;
+import com.delivery.mydelivery.database.entities.productmanage.Item;
+import com.delivery.mydelivery.database.entities.productmanage.Material;
+import com.delivery.mydelivery.database.projections.MaterialProjection;
+import com.delivery.mydelivery.database.repositories.productmanage.ItemRepository;
+import com.delivery.mydelivery.database.services.ordermanage.OrderService;
+import com.delivery.mydelivery.dto.productmanage.ItemDTO;
+import com.delivery.mydelivery.dto.productmanage.ItemMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+    @Autowired
+    ItemRepository itemRepository;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    MaterialService materialService;
+
+    public Item createItem(ItemDTO itemDTO) {
+        ClientOrder order = orderService.getClientOrderById(itemDTO.getOrderId());
+        Material material = materialService.getMaterialById(itemDTO.getMaterialId());
+        Item item = ItemMapper.fromDTO(itemDTO);
+        item.setMaterial(material);
+        item.setOrder(order);
+        return itemRepository.save(item);
+    }
+    public List<ItemDTO> getItemsByOrderId(Long orderId) {
+        return itemRepository.findItemsByOrderId(orderId);
+    }
+}
---
+++ src/main/java/com/delivery/mydelivery/dto/productmanage/ItemMapper.java
@@ -0,0 +1,36 @@
+package com.delivery.mydelivery.dto.productmanage;
+
+import com.delivery.mydelivery.database.entities.productmanage.Item;
+
+public class ItemMapper {
+    public static ItemDTO toDTO(Item item) {
+        ItemDTO itemDTO = new ItemDTO();
+        itemDTO.setId(item.getId());
+
+        itemDTO.setSize(item.getSize());
+        itemDTO.setHeight(item.getHeight());
+        itemDTO.setWidth(item.getWidth());
+
+        itemDTO.setPrice(item.getPrice());
+        itemDTO.setPricePerUnit(item.getPricePerUnit());
+        itemDTO.setAdditionalPrice(item.getAdditionalPrice());
+
+        itemDTO.setComment(item.getComment());
+
+        itemDTO.setMaterialId(item.getMaterial().getId());
+        itemDTO.setMaterialName(item.getMaterial().getName());
+        itemDTO.setOrderId(item.getOrder().getId());
+        return itemDTO;
+    }
+    public static Item fromDTO(ItemDTO itemDTO) {
+        Item item = new Item();
+        item.setSize(itemDTO.getSize());
+        item.setWidth(itemDTO.getWidth());
+        item.setHeight(itemDTO.getHeight());
+        item.setPrice(itemDTO.getPrice());
+        item.setPricePerUnit(itemDTO.getPricePerUnit());
+        item.setAdditionalPrice(itemDTO.getAdditionalPrice());
+        item.setComment(itemDTO.getComment());
+        return item;
+    }
+}

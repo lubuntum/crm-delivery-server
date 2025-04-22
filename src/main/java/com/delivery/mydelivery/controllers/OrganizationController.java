bcf541269a8e92dbb002ff51177509ca2fabd8ec
lubuntum <lubuntum@gmail.com>
2025-04-22 19:37:52 +0700

    Добавлен метод для получеиня материалов в используемых в организации

--- src/main/java/com/delivery/mydelivery/controllers/OrganizationController.java
+++ src/main/java/com/delivery/mydelivery/controllers/OrganizationController.java
@@ -1,8 +1,10 @@
 package com.delivery.mydelivery.controllers;
 
 import com.delivery.mydelivery.database.entities.ordermanage.ClientOrder;
+import com.delivery.mydelivery.database.projections.MaterialProjection;
 import com.delivery.mydelivery.database.services.accountmanage.AccountService;
 import com.delivery.mydelivery.database.services.ordermanage.OrderService;
+import com.delivery.mydelivery.database.services.productmanage.MaterialService;
 import com.delivery.mydelivery.dto.ordermanage.ClientOrderDTO;
 import com.delivery.mydelivery.services.jwt.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,8 @@ public class OrganizationController {
     @Autowired
     AccountService accountService;
     @Autowired
+    MaterialService materialService;
+    @Autowired
     JwtService jwtService;
     //TODO may be get orders depend on token -> account.employee.organization.id
     // cos its hard image just send org id, where we can store and get.
@@ -27,5 +31,10 @@ public class OrganizationController {
         return ResponseEntity.ok(orderService.getAllOrdersByOrganizationId(
                 accountService.getOrganizationByAccountId(
                         Long.valueOf(jwtService.extractSubject(token))).getId()));
+    }
+    @GetMapping("/materials")
+    public ResponseEntity<List<MaterialProjection>> getMaterialsByOrganization(@RequestHeader("Authorization") String token) {
+        return ResponseEntity.ok(materialService.getMaterialsByOrganizationId(
+                accountService.getOrganizationByAccountId(Long.valueOf(jwtService.extractSubject(token))).getId()));
     }
 }

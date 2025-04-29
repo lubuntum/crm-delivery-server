07085bf305012caf44b416ce01f38ff0dff41f90
lubuntum <lubuntum@gmail.com>
2025-04-29 19:49:13 +0700

    Entity для работы с типами оплаты

---
+++ src/main/java/com/delivery/mydelivery/database/entities/ordermanage/payment/PaymentMethod.java
@@ -0,0 +1,21 @@
+package com.delivery.mydelivery.database.entities.ordermanage.payment;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payment_method")
+public class PaymentMethod {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name")
+    private PaymentMethodEnum name;
+}
---
+++ src/main/java/com/delivery/mydelivery/database/entities/ordermanage/payment/PaymentMethodEnum.java
@@ -0,0 +1,6 @@
+package com.delivery.mydelivery.database.entities.ordermanage.payment;
+
+public enum PaymentMethodEnum {
+    CASHLESS,
+    CASH
+}

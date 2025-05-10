1d2b0129db5f4e45512bd86c3f2c30a832655b89
lubuntum <lubuntum@gmail.com>
2025-05-10 14:22:26 +0700

    Роли

--- src/main/java/com/delivery/mydelivery/database/repositories/accountmanage/RoleRepository.java
+++ src/main/java/com/delivery/mydelivery/database/repositories/accountmanage/RoleRepository.java
@@ -1,7 +1,9 @@
 package com.delivery.mydelivery.database.repositories.accountmanage;
 
 import com.delivery.mydelivery.database.entities.accountmanage.role.Role;
+import com.delivery.mydelivery.database.entities.accountmanage.role.RoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findRoleByName(RoleEnum name);
 }
--- src/main/java/com/delivery/mydelivery/database/services/accountmanage/RoleService.java
+++ src/main/java/com/delivery/mydelivery/database/services/accountmanage/RoleService.java
@@ -1,6 +1,8 @@
 package com.delivery.mydelivery.database.services.accountmanage;
 
+import com.delivery.mydelivery.database.entities.accountmanage.Account;
 import com.delivery.mydelivery.database.entities.accountmanage.role.Role;
+import com.delivery.mydelivery.database.entities.accountmanage.role.RoleEnum;
 import com.delivery.mydelivery.database.repositories.accountmanage.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,16 @@ import java.util.List;
 public class RoleService {
     @Autowired
     private RoleRepository roleRepository;
-
+    @Autowired
+    private AccountService accountService;
     public List<Role> getAllRoles(){
         return roleRepository.findAll();
+    }
+    public boolean checkRoleForAccountId(RoleEnum roleName, Long accountId) {
+        Role role = roleRepository.findRoleByName(roleName);
+        if (role == null)
+            throw new IllegalArgumentException("Role not found: " + roleName);
+        Account account = accountService.getAccountById(accountId);
+        return account != null && account.getRole().getId().equals(role.getId());
     }
 }

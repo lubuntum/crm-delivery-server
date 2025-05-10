38a314e38816a30705ad8aa2c928cd529c881fd2
lubuntum <lubuntum@gmail.com>
2025-05-10 14:20:16 +0700

    Для работы AuthContext, Spring Security

---
+++ src/main/java/com/delivery/mydelivery/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.delivery.mydelivery.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests(authorize -> authorize
+                        .requestMatchers("/api/**").permitAll());
+        return http.build();
+    }
+}

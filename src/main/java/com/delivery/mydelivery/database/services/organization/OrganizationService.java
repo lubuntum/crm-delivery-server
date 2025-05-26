7a5d44cdaf92fe5baa47c7f29f0914607cf325b0
lubuntum <lubuntum@gmail.com>
2025-05-26 09:39:04 +0700

    Метод для создания организации

--- src/main/java/com/delivery/mydelivery/database/services/organization/OrganizationService.java
+++ src/main/java/com/delivery/mydelivery/database/services/organization/OrganizationService.java
@@ -2,6 +2,7 @@ package com.delivery.mydelivery.database.services.organization;
 
 import com.delivery.mydelivery.database.entities.organization.Organization;
 import com.delivery.mydelivery.database.repositories.organization.OrganizationRepository;
+import com.delivery.mydelivery.dto.organization.OrganizationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +13,11 @@ public class OrganizationService {
 
     public Organization getOrganizationById(Long id) {
         return organizationRepository.findById(id).orElseThrow(NullPointerException::new);
+    }
+    public void createOrganization(OrganizationDTO organizationDTO) {
+        Organization organization = new Organization();
+        organization.setName(organizationDTO.getName());
+        organizationDTO.setId(organizationRepository.save(organization).getId());
+        organizationDTO.getDirectorData().setOrganizationId(organization.getId());
     }
 }
---
+++ src/main/java/com/delivery/mydelivery/dto/organization/OrganizationDTO.java
@@ -0,0 +1,16 @@
+package com.delivery.mydelivery.dto.organization;
+
+import com.delivery.mydelivery.dto.auth.AccountData;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrganizationDTO {
+    private Long id;
+    private String name;
+    private String createdAt;
+    private AccountData directorData;
+}

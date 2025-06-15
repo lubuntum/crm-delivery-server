7f500faa625d5e11cb596927c2c5d419c35c68bd
lubuntum <lubuntum@gmail.com>
2025-06-16 02:35:09 +0700

    Новые методы для работы администратора получение организаций + смена статуса

--- src/main/java/com/delivery/mydelivery/database/services/organization/OrganizationService.java
+++ src/main/java/com/delivery/mydelivery/database/services/organization/OrganizationService.java
@@ -6,8 +6,12 @@ import com.delivery.mydelivery.database.entities.organization.Organization;
 import com.delivery.mydelivery.database.repositories.accountmanage.ActiveStatusRepository;
 import com.delivery.mydelivery.database.repositories.organization.OrganizationRepository;
 import com.delivery.mydelivery.dto.organization.OrganizationDTO;
+import com.delivery.mydelivery.dto.organization.OrganizationMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrganizationService {
@@ -27,5 +31,18 @@ public class OrganizationService {
         organization.setOrganizationActiveStatus(activeStatus);
         organizationDTO.setId(organizationRepository.save(organization).getId());
         organizationDTO.getDirectorData().setOrganizationId(organization.getId());
+    }
+    public void disableOrganization(OrganizationDTO organizationDTO) {
+        Organization organization = organizationRepository.findById(organizationDTO.getId())
+                .orElseThrow(NullPointerException::new);
+        ActiveStatus activeStatus = activeStatusRepository.findActiveStatusByName(ActiveStatusEnum.DISABLED);
+        organization.setOrganizationActiveStatus(activeStatus);
+        organizationRepository.save(organization);
+    }
+    public List<OrganizationDTO> getAllOrganizations() {
+        List<Organization> organizationsList = organizationRepository.findAll();
+        return organizationsList.stream()
+                .map(OrganizationMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }

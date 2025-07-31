49ca737850ac51f12723703900d8fa5fda295671
lubuntum <lubuntum@gmail.com>
2025-07-31 08:44:03 +0700

    Добавлен метод для выгрузки данных по заказам организаций для админа

--- src/main/java/com/delivery/mydelivery/controllers/AdminController.java
+++ src/main/java/com/delivery/mydelivery/controllers/AdminController.java
@@ -5,7 +5,9 @@ import com.delivery.mydelivery.database.entities.accountmanage.role.RoleEnum;
 import com.delivery.mydelivery.database.entities.news.News;
 import com.delivery.mydelivery.database.services.accountmanage.AccountService;
 import com.delivery.mydelivery.database.services.news.NewsService;
+import com.delivery.mydelivery.database.services.ordermanage.OrderService;
 import com.delivery.mydelivery.database.services.organization.OrganizationService;
+import com.delivery.mydelivery.dto.ordermanage.clientorder.ClientOrderDTO;
 import com.delivery.mydelivery.dto.organization.OrganizationDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -28,6 +31,8 @@ public class AdminController {
     AccountService accountService;
     @Autowired
     NewsService newsService;
+    @Autowired
+    OrderService orderService;
     @HasRole(RoleEnum.ADMIN)
     @PostMapping("/organization/create")
     public ResponseEntity<Boolean> createOrganization(@RequestBody OrganizationDTO organizationDTO) {
@@ -54,5 +59,14 @@ public class AdminController {
                                               @RequestParam(value = "imageBanner", required = false) MultipartFile imageBanner) throws IOException {
         newsService.createNews(new ObjectMapper().readValue(newsJson, News.class), imageBanner);
         return ResponseEntity.ok().build();
+    }
+    //Test it
+    @HasRole(RoleEnum.ADMIN)
+    @GetMapping("/organization/orders-between-dates")
+    ResponseEntity<List<ClientOrderDTO>> getOrdersBetweenDatesByOrganization(@RequestHeader("Authorization") String token,
+                                                                             @RequestParam(required = true) LocalDateTime startDate,
+                                                                             @RequestParam(required = true) LocalDateTime endDate,
+                                                                             @RequestParam Long organizationId) {
+        return ResponseEntity.ok(orderService.getOrdersBetweenDatesForOrganization(startDate, endDate, organizationId));
     }
 }

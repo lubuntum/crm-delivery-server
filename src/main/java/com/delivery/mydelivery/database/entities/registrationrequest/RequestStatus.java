bb22ea9181f8a54ad99209adc5c31c7e9882967e
lubuntum <lubuntum@gmail.com>
2025-08-15 18:19:38 +0700

    Статус заявки

---
+++ src/main/java/com/delivery/mydelivery/database/entities/registrationrequest/RequestStatus.java
@@ -0,0 +1,21 @@
+package com.delivery.mydelivery.database.entities.registrationrequest;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "request_status")
+public class RequestStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name", nullable = false)
+    RequestStatusEnum name;
+}
---
+++ src/main/java/com/delivery/mydelivery/database/entities/registrationrequest/RequestStatusEnum.java
@@ -0,0 +1,7 @@
+package com.delivery.mydelivery.database.entities.registrationrequest;
+
+public enum RequestStatusEnum {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
